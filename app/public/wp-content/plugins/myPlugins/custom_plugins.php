8173b287f59e963a50ef6e9f661731b6858832b8
sultan <sultanfeto@gmail.com>
2024-10-06 01:27:15 -0500

    ve

--- app/public/wp-content/plugins/myPlugins/custom_plugins.php
+++ app/public/wp-content/plugins/myPlugins/custom_plugins.php
@@ -3,6 +3,6 @@
 Plugin name :custome plugin
 Auther: Sultan Feto
 Descripton: This plugin is for adding extara functionalities to the site
-version :2.0
+version :6
 AutherUrl: https://github.com/Sultan1992
 */
