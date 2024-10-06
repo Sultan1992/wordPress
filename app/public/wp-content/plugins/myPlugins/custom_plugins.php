56f40c4abd5864696768877656ba55545353b663
sultan <sultanfeto@gmail.com>
2024-10-06 01:14:56 -0500

    file updated

--- app/public/wp-content/plugins/myPlugins/custom_plugins.php
+++ app/public/wp-content/plugins/myPlugins/custom_plugins.php
@@ -1,4 +1,8 @@
 <?php
 /*
-
+Plugin name :custome plugin
+Auther: Sultan Feto
+Descripton: This plugin is for adding extara functionalities to the site
+version :2.0
+AutherUrl: https://github.com/Sultan1992
 */
